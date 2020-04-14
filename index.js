15f5fb7e98585e35c8b0952ffcf2e968cb2053c5
Brent Muha <800@holbertonschool.com>
2020-04-14 14:54:20 -0700

    Remove comment

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 addEventListener('fetch', event => {
-        event.respondWith(handleRequest(event.request))
-    })
-    /**
-     * Respond with hello worker text
-     * @param {Request} request
-     */
+    event.respondWith(handleRequest(event.request))
+})
+
 async function handleRequest(request) {
     const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
     const response = await fetch(url)
