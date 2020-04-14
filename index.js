9c7bbad393ce91ccef91d3b78018edcfc8b37e05
Brent Muha <800@holbertonschool.com>
2020-04-14 14:53:18 -0700

    Fetch urls

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+        event.respondWith(handleRequest(event.request))
+    })
+    /**
+     * Respond with hello worker text
+     * @param {Request} request
+     */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+    const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
+    const response = await fetch(url)
+    urls = await response.json()
+    return new Response(JSON.stringify(urls), {
+        headers: { 'content-type': 'text/plain' },
+    })
 }
