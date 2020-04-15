31babc27e7e3d26edb647234f27e3ecafd35f7b7
Brent Muha <800@holbertonschool.com>
2020-04-15 13:41:52 -0700

    Redirect to one of the URLs provided

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
     const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
     const response = await fetch(url)
-    urls = await response.json()
-    return new Response(JSON.stringify(urls), {
-        headers: { 'content-type': 'text/plain' },
-    })
+    const urls = (await response.json()).variants
+    const chosenUrl = urls[Math.floor(Math.random() * urls.length)]
+    return Response.redirect(chosenUrl)
 }
