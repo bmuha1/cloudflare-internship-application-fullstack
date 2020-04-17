1c8e1a9f52c2089e3370915dd6c9690f3a3a6e47
Brent Muha <800@holbertonschool.com>
2020-04-17 14:07:34 -0700

    Add custom title, description, url

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+class ElementHandler {
+    constructor(attribute) {
+        this.attribute = attribute
+    }
+    element(element) {
+        element.setInnerContent(this.attribute)
+        if (element.tagName == 'a') {
+            element.setAttribute('href', 'https://companion-cats.herokuapp.com/')
+        }
+    }
+}
+
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
@@ -7,5 +19,18 @@ async function handleRequest(request) {
     const response = await fetch(url)
     const urls = (await response.json()).variants
     const chosenUrl = urls[Math.floor(Math.random() * urls.length)]
-    return Response.redirect(chosenUrl)
+    let chosenResponse = await fetch(chosenUrl)
+    chosenResponse = new HTMLRewriter()
+        .on('title', new ElementHandler('Custom Title'))
+        .transform(chosenResponse)
+    chosenResponse = new HTMLRewriter()
+        .on('h1#title', new ElementHandler('Custom Variant ' + chosenUrl.slice(-1)))
+        .transform(chosenResponse)
+    chosenResponse = new HTMLRewriter()
+        .on('p#description', new ElementHandler('Custom description for variant ' + chosenUrl.slice(-1)))
+        .transform(chosenResponse)
+    chosenResponse = new HTMLRewriter()
+        .on('a#url', new ElementHandler('Visit Companion Cats'))
+        .transform(chosenResponse)
+    return chosenResponse
 }
