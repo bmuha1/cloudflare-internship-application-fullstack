985eaf754374f24f06bc0571e6483d30e9c82919
Brent Muha <800@holbertonschool.com>
2020-04-19 10:40:16 -0700

    Fix style

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 class ElementHandler {
     constructor(attribute) {
-        this.attribute = attribute
+        this.attribute = attribute;
     }
+
     element(element) {
-        element.setInnerContent(this.attribute)
-        if (element.tagName == 'a') {
-            element.setAttribute('href', 'https://companion-cats.herokuapp.com/')
+        element.setInnerContent(this.attribute);
+        if (element.tagName === 'a') {
+            element.setAttribute('href', 'https://companion-cats.herokuapp.com/');
         }
     }
 }
 
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-})
+    event.respondWith(handleRequest(event.request));
+});
 
 async function handleRequest(request) {
-    const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
-    const response = await fetch(url)
-    const urls = (await response.json()).variants
-    const chosenUrl = urls[Math.floor(Math.random() * urls.length)]
-    let chosenResponse = await fetch(chosenUrl)
+    const url = 'https://cfw-takehome.developers.workers.dev/api/variants';
+    const response = await fetch(url);
+    const urls = (await response.json()).variants;
+    const chosenUrl = urls[Math.floor(Math.random() * urls.length)];
+    let chosenResponse = await fetch(chosenUrl);
     chosenResponse = new HTMLRewriter()
-        .on('title', new ElementHandler('Custom Title'))
-        .transform(chosenResponse)
+        .on('title', new ElementHandler("Brent's Title"))
+        .transform(chosenResponse);
     chosenResponse = new HTMLRewriter()
-        .on('h1#title', new ElementHandler('Custom Variant ' + chosenUrl.slice(-1)))
-        .transform(chosenResponse)
+        .on('h1#title', new ElementHandler("Brent's Variant " + chosenUrl.slice(-1)))
+        .transform(chosenResponse);
     chosenResponse = new HTMLRewriter()
-        .on('p#description', new ElementHandler('Custom description for variant ' + chosenUrl.slice(-1)))
-        .transform(chosenResponse)
+        .on('p#description', new ElementHandler("Brent's description for variant " + chosenUrl.slice(-1)))
+        .transform(chosenResponse);
     chosenResponse = new HTMLRewriter()
         .on('a#url', new ElementHandler('Visit Companion Cats'))
-        .transform(chosenResponse)
-    return chosenResponse
+        .transform(chosenResponse);
+    return chosenResponse;
 }
